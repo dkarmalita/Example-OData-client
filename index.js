4d17a810140860f19ab994bbfde1be626118d7c8
Dmytro Karmalita <dmitriy.karmalita@gmail.com>
2017-02-24 23:17:38 +0200

    axios based cliend draft-1-ok

--- index.js
+++ index.js
@@ -1,47 +1,112 @@
-const o = require('odata');
-// https://www.npmjs.com/package/odata
-// https://github.com/janhommes/o.js/tree/master
-// http://www.odata.org/blog/OData-JavaScript-library-o.js-explained/
+/**
+ * Main Course
+ * ===========
+ * [x] migration to axios
+ * [x] get (filter)
+ * [x] get (select)
+ * [x] patch (update)
+ * [x] delete (delete)
+ *
+ * Special cases
+ * =============
+ * [x] ETag support
+ * [x] string and numeric id are acceptable both
+ * [ ] err on `return getList('/People','Gender','Male')`
+ */
+import colors from 'colors';
+import axios from 'axios';
 
-o('http://services.odata.org/V4/OData/OData.svc/Products').get((data) => {
-  console.log(data); //returns an array of Product data 
+const db = axios.create({
+  baseURL: 'http://services.odata.org/V4/TripPinService',
+  timeout: 1000,
+  headers: {'Content-Type': 'application/json'}
 });
 
-o('http://services.odata.org/V4/OData/OData.svc/Products').take(5).skip(2).get((data) => {
-  console.log(data); //An array of 5 products skipped by 2 
-});
+const findID = (id) => typeof(id)=='string'?`('${id}')`:`(${id})`;
+const filterEQ = (field,value) => `?$filter=${field} eq ` + (typeof(value)=='string'?`'${value}'`:`${value}`);
+const eTagHeader = (tag = '*') => {return {headers: { 'If-Match': tag }}}
 
-//--- 
+export const getRoot = async (setName = '/') => await db.get(setName);
+export const getItem = async (setName, id) => await db.get(setName+findID(id));
+export const getList = async (setName, field, value) => await db.get(setName+filterEQ(field,value));
+export const addItem = async (setName, data) => await db.post(setName, data);
+export const updateItem = async (setName, id, data, tag = '*') => await db.patch(setName+findID(id), data, eTagHeader(tag));
+export const deleteItem = async (setName, id, tag = '*') => await db.delete(setName+findID(id), eTagHeader(tag));
 
-var oHandler = o('http://services.odata.org/V4/(S(wptr35qf3bz4kb5oatn432ul))/TripPinServiceRW/People');
-oHandler.get(function(data) {
-    console.log(data); // data of the TripPinService/People endpoint
-});
+// <---------------------->
 
-o('http://services.odata.org/V4/(S(wptr35qf3bz4kb5oatn432ul))/TripPinServiceRW/People(\'Russell Whyte\')').get().then(function(result) {
-    result.data.FirstName = 'New Name';
-    return(result.save());
-}).then(function(result) {
-    console.log('First name is now New Name');
-});
+const onResolve = (x) => console.log(`resolved: ${x.status}\n`.magenta,x.data);
+const onReject = (x) => console.log(`rejected: ${x.response.status} - ${x.message}`.red);
 
-o().config({
-    endpoint: 'http://services.odata.org/V4/(S(ms4wufavzmwsg3fjo3eqdgak))/TripPinServiceRW/'
-});
+const test = async () => {
 
-   o('People').post({
-            UserName:'name',
-            FirstName:'foo',
-            LastName:'bar'
-    }).save().then(function(result) {
-        // Because o.js does not know which key to use, you need to initialize a new handler
-        var oHandler = o('People')
-            .find(('\'' + result.data.UserName + '\''))
-            .patch({ FirstName: 'new name' });
-        return(oHandler.save());
-    }).then(function(result) {
-        result.delete();
-        return(result.save());
-    }).then(function() {
-       console.log('A person was added, modified and deleted.');
-    });
+    //ok return getRoot();
+    //ok return getRoot('/Photos');
+    //ok return getItem('/Photos', 21)
+    //ok return getItem('/People', 'clydeguess')
+    //ok return getList('/People','FirstName', 'Clyde')
+
+    const newPerson = {
+        "UserName": '01-hoj'+Date.now(),
+        "FirstName": "Miala",
+        "LastName": "Thompson",
+        "Gender": "Female"
+    }
+    //ok return addItem('/People', newPerson)
+
+    const updatePerson = {
+        "LastName": "Anderson"
+    }
+    //ok return updateItem('/People', '01-hoj1487962426694', updatePerson)
+
+    const createAndUpdate = async () => {
+        let xid;
+        return await addItem('/People', newPerson)
+        .then( x => {
+                xid = x.data['UserName'];
+                console.log(`created: ${x.status}`.blue, x.data['UserName'])
+                return updateItem('/People', x.data['UserName'], updatePerson)
+            }
+            ,(x) => console.log(`rejected: ${x.response.status} - ${x.message}`.red)
+        )
+        .then( x => {
+                console.log(`updated: ${x.status}`.blue, xid)
+                return getList('/People','UserName', xid)
+            }
+            ,(x) => console.log(`rejected: ${x.response.status} - ${x.message}`.red)
+        ) 
+    }
+    //ok return createAndUpdate();
+
+    //ok return deleteItem('/People','01-hoj1487963199352')
+    //ok return getList('/People','FirstName', 'Miala')
+    
+    const createAndUpdateAndDelete = async () => {
+        let xid;
+        return await addItem('/People', newPerson)
+        .then( x => {
+                xid = x.data['UserName'];
+                console.log(`created: ${x.status}:`.blue, xid)
+                return updateItem('/People', x.data['UserName'], updatePerson)
+            }
+            ,(x) => console.log(`rejected: ${x.response.status} - ${x.message}`.red)
+        )
+        .then( x => {
+                console.log(`updated: ${x.status}:`.blue, xid)
+                return deleteItem('/People', xid)
+            }
+            ,(x) => console.log(`rejected: ${x.response.status} - ${x.message}`.red)
+        )
+        .then( x => {
+                console.log(`deleted: ${x.status}:`.blue, xid)
+                return getList('/People','UserName', xid)
+            }
+            ,(x) => console.log(`rejected: ${x.response.status} - ${x.message}`.red)
+        ) 
+    }
+    //ok 
+    return createAndUpdateAndDelete()
+
+}
+
+test().then(onResolve, onReject);
